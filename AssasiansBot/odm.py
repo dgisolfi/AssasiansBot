fa425db910f6ea87abe1d3344868df63201567d1
Daniel Gisolfi <daniel@daniel.pok.ibm.com>
2019-07-17 09:16:39 -0400

    ODM Docs finished

--- AssasiansBot/odm.py
+++ AssasiansBot/odm.py
@@ -1,75 +1,138 @@
 #!/usr/bin/python3
-# Author: Tyler Rimaldi
+# Author: Daniel Nicolas Gisolfi
 
 import os
 import pycouchdb
 import requests
 import json
 
-"""
-Object Document Mapper
-"""
-
 class ODM:
+    """
+    Object Document Mapper
+
+    A merge of the limited, but useful pycouchdb along with some 
+    additional features that can be leveraged through CouchDB's API.
+
+    Some of the Object Logic Pertinent to this project is also handled 
+    but remains abstracted for general use.
+
+    There are no public Attributes for this class
+    """
     def __init__(self):
         # Database Information
-        self.user = os.getenv('COUCHDB_USER', None)
-        self.password = os.getenv('COUCHDB_PASSWORD', None)
-        self.host = os.getenv('COUCHDB_HOST', None)
-        self.port = 5984#os.getenv('COUCHDB_PORT', 5984)
-        self.db_name = os.getenv('COUCHDB_DB', None)
-        self.conn_url = (
-            f'http://{self.user}:{self.password}@{self.host}:{self.port}'
+        self._user = os.getenv('COUCHDB_USER', None)
+        self._password = os.getenv('COUCHDB_PASSWORD', None)
+        self._host = os.getenv('COUCHDB_HOST', None)
+        self._port = os.getenv('COUCHDB_PORT', 5984)
+        self._db_name = os.getenv('COUCHDB_DB', None)
+        self._conn_url = (
+            f'http://{self._user}:{self._password}@{self._host}:{self._port}'
         )
         
         # create a connection
-        self.couch = self._connect()
+        self._couch = self._connect()
         # Get or create the db
-        self.db = self._getDatabase()
-        
-
+        self._db = self._getDatabase()
+    
     def _connect(self):
-        """ 
         """
-        
+        Get a new connection object to the database
+
+        Raises
+        ------
+        Cannot Connect to CouchDB
+            If the connection to the conn_url fails 
+            this will be raised
+
+        Returns
+        -------
+        connection : pycouchdb object
+            client for couchdb
+        """
         try:
-            return pycouchdb.Server(self.conn_url) 
+            return pycouchdb.Server(self._conn_url) 
         except:
             raise ValueError('Cannot Connect to CouchDB')
 
     def _getDatabase(self):
-        """ 
         """
-        if self.db_name in self.couch:
-            return self.couch.database(self.db_name)
-        else:
-            return self.couch.create(self.db_name)  
+        Gets or Creates a database within couchdb
 
-    ''' Basic interactions from pycouchdb '''
+        Returns
+        -------
+        db : pycouchdb database connection
+            a database connection for the specifed db_name
+        """
+        if self._db_name in self._couch:
+            return self._couch.database(self._db_name)
+        else:
+            return self._couch.create(self._db_name)  
+
+    """ Basic interactions from pycouchdb """
+
     def _get(self, _id):
-        return self.db.get(_id)
+        """ gets a couchdb document by _id """
+        return self._db.get(_id)
 
     def _create(self, obj):
-        return self.db.save(obj)
+        """ creates a new document with the contents of obj """
+        return self._db.save(obj)
 
     def _delete(self, _id):
-        return self.db.delete(_id)
+        """ removes a document specified by _id """
+        return self._db.delete(_id)
     
-    ''' Additional features '''
+    """ Additional Methods """
 
     def _find(self, query):
-        response = requests.post(f'{self.conn_url}/{self.db_name}/_find', json=query)
+        """
+        Runs a mango query on the couchdb instance
+
+        Parameters
+        ----------
+        query : dictionary
+            a mango query for couchdb...if you dont know it look it up
+        Raises
+        ------
+        Error while running find on given selector
+            find error from database
+        Returns
+        -------
+        docs : array
+            all docs that meet the specified query
+        """
+        response = requests.post(f'{self._conn_url}/{self._db_name}/_find', json=query)
         if response.status_code is not 200:
             raise ValueError('Error while running find on given selector')
         else:
             return response.json()['docs']
 
     def _update(self, _id, json):
+        """
+        Updates an existing document by setting the value to that 
+        of the given param
+
+        Parameters
+        ----------
+        _id : string
+            the unique document ID of the doc to be updated
+        json : dictionary
+            the value the doc should be updated to
+        Raises
+        ------
+        Error while updating document
+            update error from database
+        Returns
+        -------
+        doc : dictionary
+            the updated version of the document, or the details on why 
+            it was not updated
+        """
         doc = self._get(_id)
         json['_rev'] = doc['_rev']
 
         response = requests.put(
-            f'{self.conn_url}/{self.db_name}/{_id}', 
+            f'{self._conn_url}/{self._db_name}/{_id}', 
             json=json
         )
         if response.status_code is not 201:
@@ -77,9 +140,22 @@ class ODM:
         else:
             return response.json()
 
-    ''' Object Logic '''
+    """ Object Logic """
 
     def createGame(self, start_date):
+        """
+        Create a new active game
+
+        Parameters
+        ----------
+        start_date : datetime
+            time when the game should begin and targets sent out
+
+        Returns
+        -------
+        doc : dictionary
+            The game object along with its _id and _rev values
+        """
         game = {
             'status': 'active',
             'start_date': start_date,
@@ -90,6 +166,14 @@ class ODM:
         return self._create(game)
 
     def getGame(self):
+        """
+        Get the active game in the database
+
+        Returns
+        -------
+        doc : dictionary
+            The game object along with its _id and _rev values
+        """
         query = {
             'selector': {
                 'status': {
@@ -100,10 +184,41 @@ class ODM:
         return self._find(query)
     
     def updateGame(self, json):
+        """
+        Update the active game in the database
+
+        Parameters
+        ----------
+        json : dictionary
+            the value the doc should be updated to
+
+        Returns
+        -------
+        doc : dictionary
+            the updated version of the document, or the details on why 
+            it was not updated
+        """
         game = self.getGame()
-        self._update(game[0]['_id'], json)
+        return self._update(game[0]['_id'], json)
     
     def createPlayer(self, user_id, name, email):
+        """
+        Create a new player
+
+        Parameters
+        ----------
+        user_id : int
+           groupme user ID
+        name : string
+            groupme user name of player
+        email : string
+            provided email by user to be used for target assigments
+
+        Returns
+        -------
+        doc : dictionary
+            The player object along with its _id and _rev values
+        """
         player = {
             'user_id':user_id,
             'name': name,
@@ -114,6 +229,19 @@ class ODM:
         return self._create(player)
 
     def getPlayer(self, user_id):
+        """
+        Get a player by their user_id
+
+        Parameters
+        ----------
+        user_id : integer
+            the groupme user_id to identify all users
+
+        Returns
+        -------
+        docs : array
+            all players with that user_id, (there should only be one!)
+        """
         query = {
             'selector': {
                 'user_id': {
@@ -124,17 +252,21 @@ class ODM:
         return self._find(query)
     
     def updatePlayer(self, user_id, json):
-        player = self.getPlayer(user_id)
-        self._update(player[0]['_id'], json)
-    
+        """
+        Update a player in the database
 
-odm = ODM()
-odm.updatePlayer(2343, {
-  "_id": "3351ffb1e821403f9aeacf8e79683a3b",
-  "user_id": 2343,
-  "name": "daniel",
-  "email": "Daniel@",
-  "status": "dead",
-  "target": ""
-})
-print(odm.getPlayer(2343))
+        Parameters
+        ----------
+        user_id : integer
+            the groupme user_id to identify all users
+        json : dictionary
+            the value the doc should be updated to
+
+        Returns
+        -------
+        doc : dictionary
+            the updated version of the document, or the details on why 
+            it was not updated
+        """
+        player = self.getPlayer(user_id)
+        return self._update(player[0]['_id'], json)
